92f1c79e57f58680d495a1a10f416fba9a7a85d7
adeel-ahmad-dev <adeel.ahmed@coeus-solutions.de>
2021-02-10 18:13:04 +0500

    PD-326 Add-field-for-terms-and-conditions-in-Magento2-plugin

---
+++ Model/System/Config/Source/TermsAndConditions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Used in creating options for Product Identifier config value selection
+ *
+ * @category    Ekomi
+ * @copyright   Copyright (c) 2019 Ekomi ltd (http://www.ekomi.de)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Ekomi\EkomiIntegration\Model\System\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+/**
+ * Class TermsAndConditions
+ * @package Ekomi\EkomiIntegration\Model\System\Config\Source
+ */
+class TermsAndConditions implements ArrayInterface
+{
+    /**
+     * Option getter
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return $mod = [
+            ['value' => true, 'label' => 'Yes'],
+            ['value' => false, 'label' => 'No'],
+        ];
+    }
+}
